c43af1659b8b0524be58889a14444ed84739ba3a
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-10-30 00:47:36 -0500

    information update

--- src/main/java/git/hsusa/core/PluginActivation.java
+++ src/main/java/git/hsusa/core/PluginActivation.java
@@ -4,6 +4,10 @@ package git.hsusa.core;
  * Created by triston on 10/29/17.
  */
 
+/*
+    A loadable-plugin-event-interface
+ */
+
 public interface PluginActivation<RUNTIME> {
   Object onPluginActivate(RUNTIME runtime);
 }
