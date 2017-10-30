5e70591103478153a3e74e6986858861ccefe766
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-10-30 04:39:46 -0500

    GIT smart

--- src/main/java/git/hsusa/core/json/HTTP.java
+++ src/main/java/git/hsusa/core/json/HTTP.java
@@ -35,7 +35,7 @@ import java.util.Map.Entry;
 public class HTTP {
 
     /** Carriage return/line feed. */
-    public static final String CRLF = "\r\n";
+    public static final String CRLF = "\r" + "\n";
 
     /**
      * Convert an HTTP header string into a JSONObject. It can be a request
