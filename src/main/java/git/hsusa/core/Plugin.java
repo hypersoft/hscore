9f3b044d1a6ba2b9bcf49bdaaa4815d9793ddb89
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-10-30 00:45:53 -0500

    information update

--- src/main/java/git/hsusa/core/Plugin.java
+++ src/main/java/git/hsusa/core/Plugin.java
@@ -6,26 +6,25 @@ import java.util.HashMap;
  * Created by triston on 10/29/17.
  */
 
+/*
+    A bare-bones-extensible-plugin-system, with managed-feature-settings.
+ */
+
 public class Plugin {
 
   protected HashMap<String, Object> featureSet = new HashMap<>();
 
-  public Object getSupportFeature(String key) {
-    return featureSet.get(key);
-  }
-
-  public boolean getSupportStatus(String key) {
-    return featureSet.containsKey(key) && ((boolean)featureSet.get(key));
-  }
-
+  public boolean getSupportStatus(String key) {return featureSet.containsKey(key) && ((boolean)featureSet.get(key));}
   protected void enableSupportFeature(String key) {
     setSupportFeature(key, true);
   }
-
   protected void disableSupportFeature(String key) {
     setSupportFeature(key, false);
   }
 
+  public Object getSupportFeature(String key) {
+    return featureSet.get(key);
+  }
   protected void setSupportFeature(String key, Object value) {
     featureSet.put(key, value);
   }
