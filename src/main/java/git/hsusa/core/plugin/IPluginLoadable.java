d01fa832edde8d41f69715e4700efc41ff8ed047
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-10-30 20:05:20 -0500

    wall socket

--- src/main/java/git/hsusa/core/plugin/IPluginLoadable.java
+++ src/main/java/git/hsusa/core/plugin/IPluginLoadable.java
@@ -4,6 +4,5 @@ package git.hsusa.core.plugin;
  * Created by triston on 10/30/17.
  */
 public interface IPluginLoadable<LOADER, BUNDLE> {
-  // What you return from this, is what the plugin uses to represent itself.
   void onLoad(LOADER loader, BUNDLE bundle);
 }
