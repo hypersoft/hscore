c3dff3cc12271323ce902f75fdffc56d63a844fd
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-11-01 07:11:20 -0500

    Android Studio Testing Framework

---
+++ src/test/java/git/hsusa/core/plugin/PluginTest.java
@@ -0,0 +1,47 @@
+package git.hsusa.core.plugin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by triston on 11/1/17.
+ */
+public class PluginTest {
+
+  @Test
+  public void getSettingNames() throws Exception {
+
+  }
+
+  @Test
+  public void getPluginLoader() throws Exception {
+
+  }
+
+  @Test
+  public void getPluginName() throws Exception {
+
+  }
+
+  @Test
+  public void putSetting() throws Exception {
+
+  }
+
+  @Test
+  public void getSetting() throws Exception {
+
+  }
+
+  @Test
+  public void toJSONString() throws Exception {
+
+  }
+
+  @Test
+  public void toJSONString1() throws Exception {
+
+  }
+
+}
