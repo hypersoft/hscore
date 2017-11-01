bc23879b56070baabcf2d8aee92a6a2a152678e2
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-11-01 06:06:11 -0500

    Testing Support with archive stripping

---
+++ src/androidTest/java/git/hsusa/core/AndroidTesting.java
@@ -0,0 +1,23 @@
+package git.hsusa.core;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class AndroidTesting {
+
+  @Test
+  public void smapleTest() throws Exception {
+
+  }
+
+}
---
+++ src/test/java/git/hsusa/core/Testing.java
@@ -0,0 +1,22 @@
+package git.hsusa.core;
+
+/**
+ * Created by triston on 11/1/17.
+ */
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class Testing {
+
+  @Test
+  public void someTest() {
+
+  }
+
+}
