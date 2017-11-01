de55c62ef03a0ab2779dc2a2dc67a4718e943fae
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-10-31 21:26:01 -0500

    improved command support planning

--- src/main/java/git/hsusa/core/plugin/IPluginCommandable.java
+++ src/main/java/git/hsusa/core/plugin/IPluginCommandable.java
@@ -4,10 +4,13 @@ package git.hsusa.core.plugin;
  * Created by triston on 10/30/17.
  */
 
+import java.util.Set;
+
 /**
  * IKnowMyPurposes
  * Just claim this interface, setup your method, and you are off to the races.
  */
 public interface IPluginCommandable {
   Object doPluginCommand(String command, Object... parameters);
+  Set<String> getPluginCommands();
 }
