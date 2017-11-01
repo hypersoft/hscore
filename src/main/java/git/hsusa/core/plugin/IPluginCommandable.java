0b835f960b2da79193132409cf6e50683c523fb3
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2017-11-01 00:32:35 -0500

    code talk correction

--- src/main/java/git/hsusa/core/plugin/IPluginCommandable.java
+++ src/main/java/git/hsusa/core/plugin/IPluginCommandable.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 /**
  * IKnowMyPurposes
- * Just claim this interface, setup your method, and you are off to the races.
+ * Just claim this interface, setup your methods, and you are off to the races.
  */
 public interface IPluginCommandable {
   Object doPluginCommand(String command, Object... parameters);
